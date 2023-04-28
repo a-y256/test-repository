accf00152931671dc713babf48164733ddaed984
a-y256 <mnmnudxfex@gmail.com>
2023-04-28 15:42:14 +0900

    test1変更

--- main5.rb
+++ main5.rb
@@ -1,11 +1,3 @@
-require 'dxruby'
-
-player_img = Image.load("image/player.png")
-x = 100
-y = 100 # 追加
-
-Window.loop do
-  x = x + Input.x # 追加
-  y = y + Input.y # 追加
-  Window.draw(x, y, player_img) # 書き換え
-end
+puts"ffffsaadfa"
+puts"ffffsaadfa"
+puts"ffffsaadfa"
